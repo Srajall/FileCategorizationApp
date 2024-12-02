6889e9950dc4e202b31cb920ee4c42c939f17cd4
Srajall <88056795+Srajall@users.noreply.github.com>
2024-12-02 15:20:34 +0530

    Update DocumentmanagementApplication.java

--- documentmanagement/src/main/java/com/example/documentmanagement/DocumentmanagementApplication.java
+++ documentmanagement/src/main/java/com/example/documentmanagement/DocumentmanagementApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DocumentmanagementApplication {
 
 	public static void main(String[] args) {
+		// Set the Google Cloud credentials path
+        System.setProperty("GOOGLE_APPLICATION_CREDENTIALS", "C:/Users/2153045/Downloads/constant-disk-350520-4191821ded2c.json");
+
+	
 		SpringApplication.run(DocumentmanagementApplication.class, args);
 	}
 
